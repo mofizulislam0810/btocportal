872ab9e014316a5fc094a76e21d464b424964f25
mofizulislam0810 <mofizul.islam0810@gmail.com>
2022-07-31 10:02:13 +0600

    31-07-2022(1)

--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Packages.css";
 import { Link } from "react-router-dom";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Packages = () => {
   let packageList = [
@@ -36,56 +38,145 @@ const Packages = () => {
       title: "KUALA LAMPUR - LANGKAWI 5 DAYS 4 NIGHTS",
       totalPrice: 12500,
     },
+    {
+      packID: 1004,
+      bannerImg:
+        "https://media.cntraveller.com/photos/6193e2965502655976f78f59/16:9/w_5120,h_2880,c_limit/Malaysia-503847689-conde-nast-traveller-16nov21%20Getty.jpg",
+      locationName: "Malaysia",
+      title: "KUALA LAMPUR - LANGKAWI 5 DAYS 4 NIGHTS",
+      totalPrice: 12500,
+    },
+    {
+      packID: 1004,
+      bannerImg:
+        "https://media.cntraveller.com/photos/6193e2965502655976f78f59/16:9/w_5120,h_2880,c_limit/Malaysia-503847689-conde-nast-traveller-16nov21%20Getty.jpg",
+      locationName: "Malaysia",
+      title: "KUALA LAMPUR - LANGKAWI 5 DAYS 4 NIGHTS",
+      totalPrice: 12500,
+    },
+    {
+      packID: 1004,
+      bannerImg:
+        "https://media.cntraveller.com/photos/6193e2965502655976f78f59/16:9/w_5120,h_2880,c_limit/Malaysia-503847689-conde-nast-traveller-16nov21%20Getty.jpg",
+      locationName: "Malaysia",
+      title: "KUALA LAMPUR - LANGKAWI 5 DAYS 4 NIGHTS",
+      totalPrice: 12500,
+    },
+    {
+      packID: 1004,
+      bannerImg:
+        "https://media.cntraveller.com/photos/6193e2965502655976f78f59/16:9/w_5120,h_2880,c_limit/Malaysia-503847689-conde-nast-traveller-16nov21%20Getty.jpg",
+      locationName: "Malaysia",
+      title: "KUALA LAMPUR - LANGKAWI 5 DAYS 4 NIGHTS",
+      totalPrice: 12500,
+    },
   ];
+
   return (
     <>
-      <h2 className="text-dark fw-bold text-start pt-5 container">Holiday Packages</h2>
-      <section className="pb-5">
+      <h2 className="text-dark fw-bold text-start pt-5 container">
+        Holiday Packages
+      </h2>
+      <section>
         <div id="cards_landscape_wrap-2">
           <div className="container">
-            <div className="row">
-              {packageList.map((item, index) => {
-                return (
-                  <div
-                    className="col-xs-12 col-sm-6 col-md-3 col-lg-3"
-                    key={index}
-                  >
-                    <Link to={`/packagedetails/${item.packID}`}>
-                      <div className="card-flyer">
-                        <div className="text-box">
-                          <div className="image-box">
-                            <img src={item.bannerImg} alt="" />
-                          </div>
-                          <div className="text-container text-start ">
-                            <h6>
-                              <i
-                                className="fa fa-map-marker me-1"
-                                aria-hidden="true"
-                              ></i>
-                              {item.locationName}
-                            </h6>
-                            <h6
-                              className="fw-bold mb-0 text-secondary"
-                              style={{ fontSize: "9px" }}
-                            >
-                              {item.title}
-                            </h6>
-                            <p className="fw-bold m-0 text-secondary">
-                              <span style={{ fontSize: "9px" }}>
-                                Starts From
-                                <span className="ms-1" style={{ fontSize: "13px" }}>
-                                  BDT {item.totalPrice}
+            <div className="">
+              <Carousel
+                additionalTransfrom={0}
+                arrows
+                autoPlaySpeed={3000}
+                centerMode={false}
+                className=""
+                containerClass="container-with-dots"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={80}
+                pauseOnHover
+                renderArrowsWhenDisabled={false}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                  desktop: {
+                    breakpoint: {
+                      max: 3000,
+                      min: 1024,
+                    },
+                    items: 4,
+                    partialVisibilityGutter: 40,
+                  },
+                  mobile: {
+                    breakpoint: {
+                      max: 464,
+                      min: 0,
+                    },
+                    items: 1,
+                    partialVisibilityGutter: 30,
+                  },
+                  tablet: {
+                    breakpoint: {
+                      max: 1024,
+                      min: 464,
+                    },
+                    items: 2,
+                    partialVisibilityGutter: 30,
+                  },
+                }}
+                rewind={false}
+                rewindWithAnimation={false}
+                rtl={false}
+                shouldResetAutoplay
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
+              >
+                {packageList.map((item, index) => {
+                  return (
+                    <div className="px-2 mb-5" key={index}>
+                      <Link to={`/packagedetails/${item.packID}`}>
+                        <div className="card-flyer">
+                          <div className="text-box">
+                            <div className="image-box">
+                              <img src={item.bannerImg} alt="" />
+                            </div>
+                            <div className="text-container text-start ">
+                              <h6>
+                                <i
+                                  className="fa fa-map-marker me-1"
+                                  aria-hidden="true"
+                                ></i>
+                                {item.locationName}
+                              </h6>
+                              <h6
+                                className="fw-bold mb-0 text-secondary"
+                                style={{ fontSize: "9px" }}
+                              >
+                                {item.title}
+                              </h6>
+                              <p className="fw-bold m-0 text-secondary">
+                                <span style={{ fontSize: "9px" }}>
+                                  Starts From
+                                  <span
+                                    className="ms-1"
+                                    style={{ fontSize: "13px" }}
+                                  >
+                                    BDT {item.totalPrice}
+                                  </span>
+                                  PP
                                 </span>
-                                PP
-                              </span>
-                            </p>
+                              </p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })}
+                      </Link>
+                    </div>
+                  );
+                })}
+              </Carousel>
             </div>
           </div>
         </div>
